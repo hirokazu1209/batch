ad60755177df38454a0b0560ae17e96843e29ec0
hirokazu <n.hirokazu92@gmail.com>
2022-09-11 15:25:01 +0900

    feat seedファイルの実装

--- db/migrate/20220911041537_create_ranks.rb
+++ db/migrate/20220911041537_create_ranks.rb
@@ -2,6 +2,7 @@ class CreateRanks < ActiveRecord::Migration[6.0]
   def change
     create_table :ranks, comment: 'ゲーム内のランキング情報' do |t|
       # index: { unique: true } ...user_id1種類につき、ranksテーブルには1行のみ格納可能
+      # on_restrict ...子テーブルから先にデータを削除しないとエラーになる様に設定する
       t.references :user, null: false, index: { unique: true }, foreign_key: { on_delete: :restrict, on_update: :restrict }, comment: 'ユーザー'
       t.integer :rank, null: false, default: 0, index: true, comment: 'ユーザーの順位'
       t.integer :score, null: false, default: 0, comment: 'このランクに至ったスコア'
